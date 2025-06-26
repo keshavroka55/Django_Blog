9e7f7b46525a0483a0b8bd6e44857b33c6da8774
keshav <keshavrokaya1244@gmail.com>
2025-06-26 15:53:33 +0545

    last commit of this project

--- keshav/admin.py
+++ keshav/admin.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
-# from keshav.models import Tweet
 from keshav.models import Message
 
-
 # Register your models here.
-# admin.site.register(Tweet)
 admin.site.register(Message)
 
 
--- keshav/models.py
+++ keshav/models.py
@@ -6,18 +6,6 @@ from django.core.validators import MaxLengthValidator
 
 
 
-    
-# class Tweet(models.Model):
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
-#     text = models.TextField(max_length=240)
-#     photo = models.ImageField(upload_to='photos/', blank=True,null=True)
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
-
-#     def __str__(self):
-#         return f"{self.user.username} -   {self.text[:10]}"
-    
-
 class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField(validators=[MaxLengthValidator(240)])
