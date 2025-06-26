0ef89734bbe869466d1d88c1e517aceb2414c93d
keshav <keshavrokaya1244@gmail.com>
2025-06-26 13:25:00 +0545

    final before pushing it on render

--- My_first/settings.py
+++ My_first/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-import django_heroku
+
 from pathlib import Path
 import os
 
@@ -167,7 +167,5 @@ LOGOUT_REDIRECT_URL = '/keshav/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# activate dajngo heroku.
-django_heroku.settings(locals())
 
 
