83e985557f78f3262e87051510e1443d4ee1f62b
keshav <keshavrokaya1244@gmail.com>
2025-06-30 07:46:35 +0545

    somechanges

--- My_first/views.py
+++ My_first/views.py
@@ -1,20 +1,23 @@
 
 from django.http import HttpResponse
 from django.shortcuts import render
-from keshav.models import Keshav
+from keshav.models import Keshav 
 
 def load(request):
     blogs = Keshav.objects.all().order_by('-created_at')
     return render (request, 'load.html',{'blogs':blogs})
 
-def home(request):
-    return render(request,'home.html')
-
 def login_sucess(request):
     return render(request,'login_sucess.html')
 
 def keshav_list(request):
+    # q = request.GET.get('q') if request.GET.get('p') != None else ''
+    # blogs = Keshav.objects.filter(category__icontains=q)
+
     blogs = Keshav.objects.all().order_by('-created_at')
-    return render(request, 'keshav_list.html', {'blogs': blogs})
+
+    # user = Keshav.objects.all()
+    context = {'blogs': blogs}
+    return render(request, 'keshav_list.html', context)
 
 
