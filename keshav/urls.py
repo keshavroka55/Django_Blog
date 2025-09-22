703137743d16f156a4c75c7ee6c7f86534de745b
keshav <keshavrokaya1244@gmail.com>
2025-09-23 04:31:53 +0545

    testing

--- keshav/urls.py
+++ keshav/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path, include
 from . import views
-from django.contrib.auth import views as auth_views
-from django.contrib.auth.views import PasswordChangeView
 
 
 urlpatterns = [
